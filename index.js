ffe5c9f0ee47e2f0406a7adbfcab5b7210685d09
Daniel Schoch <d.schoch90@gmail.com>
2018-07-27 18:54:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
 // Add your functions and code here
-var kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten (name) {
-  kittens.push(name)
-  return kittens
+  kittens.push(name);
+  return kittens;
 }
 
-kittens = ["Milo", "Otis", "Garfield"]
+kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyRemoveLastKitten () {
+  kittens.pop();
+  return kittens;
+}
+
+kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyRemoveFirstKitten () {
+  kittens.shift();
+  return kittens;
+}
+
+kittens = ["Milo", "Otis", "Garfield"];
+
+function appendkitten(name) {
+  
+}
+
+function prependKitten (name) {
+  
+}
+
+function removeLastKitten() {
+  
+}
+
+function removeFirstKitten() {
+  
+}
